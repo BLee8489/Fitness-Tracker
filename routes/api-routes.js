0718987cbef9e622781385dd5babc2d8e43643a9
“bonnielee” <bonlee8489@gmail.com>
2021-03-18 14:46:17 -0700

    edits in routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,86 +1,83 @@
-const Workout = require("../models/workout")
+const db = require("../models");
 
 module.exports = (app) => {
-    // Find last workout 
-    app.get("/api/workouts", (req, res) => {
-        // console.log(req.body)
-        Workout.find({}) 
-        Workout.aggregate([
-            {"$addFields": {
-                "totalDuration":{
-                    "$sum": "$exercises.duration"
-                }
-            }}
-        ])
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+    db.Workout.aggregate([
+      {
+        "$addFields": {
+          "totalDuration": {
+            "$sum": "$exercises.duration",
+          },
+        },
+      },
+    ])
 // Sort date
-        .sort({ date: -1 })
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-    });
-    // to add exercise
-    app.put("/api/workouts/:id", (req, res) => {
-        // console.log(req.body, req.params.id)
-        Workout.findOneAndUpdate(
-            {_id: req.params.id},
-            {
-                $push: { 
-                    exercises: req.body
-                    
-                },
-                
-            },
-            {new: true},
-        )
-        .then(data => {
-            // console.log(data)
-           res.json(data);
+.sort({ date: -1 })
+.then((dbWorkout) => {
+  res.json(dbWorkout);
+})
+.catch((err) => {
+  res.json(err);
+});
+});
 
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+// Create a workout
+  app.post("/api/workouts", (req, res) => {
+    db.Workout.create({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
-    });
-    // Create a workout
-    app.post("/api/workouts", (req, res) => {
-        // console.log(req.body, req.params)
-        Workout.create(
-            {
-                exercises: req.body.exercises,
-                day: Date.now(),
-            },
-        )
-        .then(data => {
-            // console.log("DATA: " + data)
-            res.json(data);
-            // res.send("hello from create post")
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-    });
+  // Get request for workouts 
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+    db.Workout.aggregate([
+      {
+        "$addFields": {
+          "totalDuration": {
+            "$sum": "$exercises.duration",
+          },
+        },
+      },
+    ])
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
-    app.get("/api/workouts/range", (req, res) => {
-        Workout.aggregate([
-            {"$addFields": {
-                "totalDuration":{
-                    "$sum": "$exercises.duration"
-                }}
-            }
-        ])
-        // Get last 7 exercises.
-        // Sort date in descending order; limit 7 exercises, sort day in ascending order
-        .sort({ day: -1 }).limit(7).sort({ day: 1 })
-        .then(data => {
-            // console.log(data)
-            res.json(data);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-    })
+  app.post("/api/workouts/range", (req, res) => {
+    db.Workout.create({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: { exercises: req.body },
+      },
+      {
+        new: true,
+      }
+    )
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 };
