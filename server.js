c86176cd9474cdc4d3b48009efa175491447fb7b
“bonnielee” <bonlee8489@gmail.com>
2021-03-18 15:07:30 -0700

    edits to server.js

--- server.js
+++ server.js
@@ -1,31 +1,28 @@
-// adding dependencies
 const express = require("express");
-const mongoose = require("mongoose");
 const logger = require("morgan");
-const dotenv = require("dotenv").config();
-
-const app = express();
-app.use(logger("dev"));
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-// setting up middleware
+const app = express();
+
+app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
-// connecting to database
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
-// connecting routes
-require("./routes/html-routes");
-require("./routes/api-routes");
-
+require("./routes/html-routes")(app);
+require("./routes/api-routes")(app);
 
 app.listen(PORT, () => {
-    console.log(`App running on PORT http://localhost:${PORT}`);
+  console.log(`App running on port http://localhost:${PORT}`);
 });
